45613cac0f4ebb1c8dfd7f48cce6ecc1be411070
blackehacker999 <np1044769@gmail.com>
2025-06-15 10:29:03 +0530

    telegram bot

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Chat-id-bot
-Chat id bot for telegram by amarjit_999 
+Chat id bot for telegram by amarjit 
+
+
+## installation
+
+
+'''bash
+ git clone https://github.com/blackehacker999/Chat-id-bot.git
+ cd Chat-id-bot
+ apt install python
+ pip install telebot
+
+  '''
+ > open bot.py with any text editor and add your bot token
+
+run the bot.py 'python bot.py'
+
---
+++ bot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pythonMore actions
+
+import telebot
+bot_token = "Your bot token" # add your telegram bot token
+
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['start'])
+
+def wlcm_msg(message):
+        wlcm_text = """welcom to chat id bot send /id command to know your id
+            created by @alienkrishn"""" # add your username 
+                created by @alienkrishn""" # add your username 
+                    bot.reply_to(message, wlcm_text)
+
+                    @bot.message_handler(commands=['id'])
+                    def send_id(message):
+                            chat_id = message.chat.id 
+                                bot.reply_to(message, f"Your chat id is : {chat_id}")
+                                bot.polling()
